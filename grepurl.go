84b7f4c03fc0cfe1a5fbbe60600218ffe7ba95e8
Arne Neumann <github@arne.cl>
2019-10-22 23:05:52 +0200

    extract URLs from remote html file (http)

--- grepurl.go
+++ grepurl.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	//"io/ioutil"
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
@@ -26,34 +31,24 @@ func printImageURL(token html.Token) {
 	}
 }
 
-func main() {
-	// TODO: check if it is a file
-	filepath := os.Args[1]
-	file, err := os.Open(filepath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Can't read file '%s': %v\n", filepath, err)
-		os.Exit(1)
-	}
-
-	tokenizer := html.NewTokenizer(file)
-
+func printAllURLs(t *html.Tokenizer) {
 	for {
-		tokenType := tokenizer.Next()
+		tokenType := t.Next()
 
 		//if it's an error token, we either reached
 		//the end of the file, or the HTML was malformed
 		if tokenType == html.ErrorToken {
-			err := tokenizer.Err()
+			err := t.Err()
 			if err == io.EOF {
 				break //end of file, break out of the loop
 			}
 
 			//malformed HTML is "normal", so we'll just continue
-			fmt.Fprintf(os.Stderr, "error tokenizing HTML: %v", tokenizer.Err())
+			fmt.Fprintf(os.Stderr, "error tokenizing HTML: %v", t.Err())
 			continue
 		}
 
-		token := tokenizer.Token()
+		token := t.Token()
 		if tokenType == html.StartTagToken {
 			if token.DataAtom == atom.A { // <a> element
 				printAnchorURL(token)
@@ -64,4 +59,75 @@ func main() {
 			}
 		}
 	}
+}
+
+// htmlTokenizerFromFilePath opens a local HTML file
+// and returns a tokenizer of it.
+func htmlTokenizerFromFilePath(path string) *html.Tokenizer {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("Can't read file '%s': %v\n", path, err)
+	}
+	return html.NewTokenizer(file)
+}
+
+// htmlTokenizerFromURL takes the URL of an HTML file, downloads it and
+// returns a tokenizer of it.
+func htmlTokenizerFromURL(url string) *html.Tokenizer {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatalf("error fetching URL: %v\n", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("response status code was %d\n", resp.StatusCode)
+	}
+
+	ctype := resp.Header.Get("Content-Type")
+	if !strings.HasPrefix(ctype, "text/html") {
+		log.Fatalf("response content type was %s not text/html\n", ctype)
+	}
+
+	//make sure the response body gets closed
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	if nil != err {
+		fmt.Println("Error reading response body: ", err.Error())
+	}
+
+	r := bytes.NewReader(body)
+	return html.NewTokenizer(r)
+}
+
+// isValidURL tests a string to determine if it is a well-structured URL.
+// cf. https://stackoverflow.com/questions/31480710/validate-url-with-standard-package-in-go
+func isValidURL(str string) bool {
+	u, err := url.Parse(str)
+	return err == nil && u.Scheme != "" && u.Host != ""
+}
+
+// isValidFilePath checks if a file exists and is not a directory.
+func isValidFilePath(path string) bool {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func main() {
+	path := os.Args[1]
+
+	var tokenizer *html.Tokenizer
+	if isValidURL(path) {
+		tokenizer = htmlTokenizerFromURL(path)
+	} else if isValidFilePath(path) {
+		tokenizer = htmlTokenizerFromFilePath(path)
+	} else {
+		log.Fatalf("Path '%v' is not a valid URL / file path.", path)
+	}
+
+	printAllURLs(tokenizer)
 }
